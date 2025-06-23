d54ae1fec55fa9780f127e451ab5b886b986e0fb
vitorelena <vitoriahelena.311005@gmail.com>
2025-06-23 11:36:35 -0300

    Correção Model (João percebeu o erro na classe outros e funcioario.py)
    Criação do main

--- website/model/Produtos/Outros.py
+++ website/model/Produtos/Outros.py
@@ -1,23 +1,20 @@
 from website.model.Produtos.Produto import Produto
 from ... import db
 
-class Equipamentos(Produto):
+class Outros(Produto):
     __mapper_args__ = {
-        'polymorphic_identity': '4'
+        'polymorphic_identity': '6'
     }
 
-    tipo_equipamento = db.Column(db.String(50))
-    marca = db.Column(db.String(30))
+    detalhes = db.Column(db.String(100))
 
     def __init__(self, nome, descricao, preco, codigo_de_barras, imagem_url, **kwargs):
         super().__init__(
             nome=nome, descricao=descricao, preco=preco, codigo_de_barras=codigo_de_barras,
-            imagem_url=imagem_url, categoria='4'
+            imagem_url=imagem_url, categoria='6'
         )
-        self.tipo_equipamento = kwargs.get('tipo_equipamento')
-        self.marca = kwargs.get('marca_equipamento')
+        self.detalhes = kwargs.get('detalhes_outros')
 
     def gerar_relatorio(self):
         base = super().gerar_relatorio()
-        # Use o atributo com o nome da coluna específica
-        return f"{base}\nTipo: {self.tipo_equipamento}\nMarca: {self.marca}"
+        return f"{base}\nDetalhes: {self.detalhes}"
